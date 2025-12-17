756309f1612d4466d4a332a2f477d42d0ab4eff1
sitik <sitik@DESKTOP-0SF1HK7>
2025-12-17 09:40:29 +0200

    Add project files.

---
+++ Dismissal_Appointment/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Dismissal_Appointment
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Dismissal_Appointment/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Dismissal_Appointment
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
